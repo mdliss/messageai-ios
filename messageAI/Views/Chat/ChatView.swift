95b69445b10833630cac3214da1f405190e113b1
Max Liss <maxliss19@gmail.com>
2025-10-21 22:13:08 -0500

    Revert "working MVP version w/ notif"
    This reverts commit 5387c90c4441e8e8e59e4e5a81c3d829188a653d.

--- messageAI/Views/Chat/ChatView.swift
+++ messageAI/Views/Chat/ChatView.swift
@@ -268,9 +268,6 @@ struct ChatView: View {
         let text = messageText.trimmingCharacters(in: .whitespacesAndNewlines)
         guard !text.isEmpty else { return }
         
-        // Clear input immediately (optimistic UI)
-        messageText = ""
-        
         // Get current user info from conversation
         let participant = conversation.participantDetails[currentUserId]
         
